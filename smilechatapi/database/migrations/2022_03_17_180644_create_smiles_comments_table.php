1a1963408b6fd53fb5b327e0141e6b32716564ea
unknown <timotej.trsavec@sers.si>
2022-03-25 10:22:45 +0100

    popravljene vse napake v migracijah

--- smilechatapi/database/migrations/2022_03_17_180644_create_smiles_comments_table.php
+++ smilechatapi/database/migrations/2022_03_17_180644_create_smiles_comments_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->bigIncrements('id');
             $table->bigInteger("user_id")->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
-            $table->bigInteger('comment_id');
+            $table->bigInteger('comment_id')->unsigned();
             $table->foreign('comment_id')->references('id')->on('comments');
             $table->boolean('smile');
             $table->timestamps();
