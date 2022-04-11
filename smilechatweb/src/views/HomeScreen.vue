6828448c1a0103ba15dcaad1fb9c0541d21da083
Jovan Gugunovic <jovangugunovic@gmail.com>
2022-04-11 14:47:27 +0200

    Delo na profile screen in home screen

--- smilechatweb/src/views/HomeScreen.vue
+++ smilechatweb/src/views/HomeScreen.vue
@@ -1,15 +1,20 @@
 <template>
 <v-card
-    color="grey lighten-4"
+    color="white"
     flat
     height="50px"
     tile
   >
     <v-toolbar dense>
-
       <v-toolbar-title>SmileChat</v-toolbar-title>
-
       <v-spacer></v-spacer>
+ <v-text-field
+        hide-details
+        prepend-icon="mdi-magnify"
+        single-line
+        justify
+      ></v-text-field>
+ <v-spacer></v-spacer>
 
       <v-btn icon>
         <v-icon>mdi-home</v-icon>
@@ -30,24 +35,41 @@
       <v-btn icon>
         <v-icon>mdi-account</v-icon>
       </v-btn>
+      
     </v-toolbar>
+    
        <v-card
     class="mx-auto"
-    color="#fff"
-    white
+    color="#white"
     max-width="800"
     height="60"
   >
-  <v-card-title>
+  <v-card-title class="d-flex justify-space-between" >
       <v-icon
         large
         left
       >
         mdi-account
+
       </v-icon>
        <v-icon
         large
         left
+        v-list-item-avatar color="blue"
+      >
+     
+        mdi-account
+      </v-icon>
+        <v-icon
+        large
+        left
+      >
+        mdi-account
+      </v-icon>
+        <v-icon
+        large
+        left
+        v-list-item-avatar color="blue"
       >
         mdi-account
       </v-icon>
@@ -60,18 +82,7 @@
         <v-icon
         large
         left
-      >
-        mdi-account
-      </v-icon>
-        <v-icon
-        large
-        left
-      >
-        mdi-account
-      </v-icon>
-        <v-icon
-        large
-        left
+         v-list-item-avatar color="blue"
       >
         mdi-account
       </v-icon>  <v-icon
@@ -86,17 +97,20 @@
         large
         left
       >
-        mdi-account
       </v-icon>
-      <span class="text-h6 font-weight-light">Jovan</span>
     </v-card-title>
   <v-card
+  
     class="mx-auto"
     color="#fff"
     white
     max-width="800"
     height="500"
   >
+  <v-icon>
+     mdi-account 
+      </v-icon>
+    <span class="text-h7 font-weight-light">Jovan</span>
     <v-card-title>
       <v-icon
         large
@@ -104,10 +118,12 @@
       >
       </v-icon>
     </v-card-title>
-
-    <v-card-text class="text-h5 font-weight-bold">
-      "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."
-    </v-card-text>
+    <v-img
+  lazy-src="https://picsum.photos/id/11/10/6"
+  max-height="500"
+  max-width="800"
+  src="https://picsum.photos/id/11/500/300"
+></v-img>
 
     <v-card-actions>
       <v-list-item class="grow">
@@ -115,29 +131,34 @@
           align="center"
           justify=""
         >
-          <v-icon class="mr-2">
-            mdi-emoticon
-          </v-icon>
+          <v-btn icon>
+        <v-icon>mdi-emoticon</v-icon>
+      </v-btn>
           <span class="mr-2"></span>
-          <v-icon class="mr-2">
-           mdi-comment
-          </v-icon>
-           <v-icon class="mr-2">
-           mdi-share-variant
-          </v-icon>
+          <v-btn icon>
+        <v-icon>mdi-comment</v-icon>
+      </v-btn>
+           <v-btn icon>
+        <v-icon>mdi-share-variant</v-icon>
+      </v-btn>
         </v-row>
       </v-list-item>
     </v-card-actions>
+ <v-card-subtitle class="text-h7 font-weight-gray">
+      1600 smiles
+    </v-card-subtitle>
+    <v-card-text class="text-h7 font-weight-black">
+      Turns out semicolon-less style is easier and safee.....more
+    </v-card-text>
+      <v-card-text class="text-h7 font-weight-gray">
+      View all 100 comments
+    </v-card-text>
+ <v-card-text class="text-h7 font-weight-gray">
+      1 hour ago
+    </v-card-text>
   </v-card>
-
-
-
-
-
-
   </v-card>
-</v-card>
-
+       </v-card>
 </template>
 
 <script>
---
+++ smilechatweb/src/views/Profile.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app id="/reg">
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="8">
+           
+           
+            <v-card
+              class="mx-auto"
+              color="#ECECEC"
+              light
+              max-width="1200"
+              width="1200"
+            >
+            <v-row>
+                <v-card-title primary-titles class="justify-left">
+                  <v-icon  light size="200" >mdi-account-circle</v-icon> 
+                </v-card-title>
+              <v-list-item-content>
+                <v-list-item-title-font-size>
+                    <v-list-item-title class="text-h5 font-weight-bold" align="left">gugunovic_official</v-list-item-title>
+                    <v-list-item-title class="text-h5 font-weight" align="left">8 Posts  &nbsp;&nbsp;&nbsp;&nbsp; 46 Followers &nbsp;&nbsp;&nbsp;&nbsp; 4 Following</v-list-item-title>
+                </v-list-item-title-font-size>
+              </v-list-item-content>
+            </v-row>
+              <v-row>
+              <v-col
+                v-for="n in 9"
+                :key="n"
+                class="d-flex child-flex"
+                cols="4"
+              >
+                <v-img
+                  :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+                  :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+                  aspect-ratio="1"
+                  class="grey lighten-2"
+                >
+                  <template v-slot:placeholder>
+                    <v-row
+                      class="fill-height ma-0"
+                      align="center"
+                      justify="center"
+                    >
+                      <v-progress-circular
+                        indeterminate
+                        color="grey lighten-5"
+                      ></v-progress-circular>
+                    </v-row>
+                  </template>
+                </v-img>
+              </v-col>
+            </v-row>
+
+          </v-card>
+
+
+
+          </v-col>
+        </v-row>
+      </v-container>
+  </v-app>
+</template>
